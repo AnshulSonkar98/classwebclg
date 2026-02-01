5e414320e7b2a1f0c248cf3a18fa56bf066b75de
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-02-01 23:42:01 +0530

    initial

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let canvas=   document.querySelector("canvas")
+  let canvas=   document.querySelector("canvas")
   let pen=  canvas.getContext("2d")
    pen.fillStyle='gold'
    let cell=50
    let cellQ=[[0,0]]
+   let count=0
    let gameOver=false
    let direction='right'
    let randomC=generateRandomCell()
@@ -10,9 +11,9 @@ let canvas=   document.querySelector("canvas")
  let id=  setInterval(()=>{
     draw()
     update()
-   },400)
+   },200)
      document.addEventListener("keydown",function(e){
-        // console.log(e);
+      
         if(e.key=='ArrowDown'){
             direction='down'
         }else if(e.key=='ArrowUp'){
@@ -26,17 +27,21 @@ let canvas=   document.querySelector("canvas")
      })
 
    function draw(){
-    // for()
+    
     if(gameOver==true){
         clearInterval(id)
         return;
     }
-       pen.fillStyle='red'
-    pen.clearRect(0,0,1000,600)
+       pen.fillStyle='#b6ad90'
+    pen.clearRect(0,0,1000,800)
     for(let i of cellQ){
         pen.fillRect(i[0],i[1],cell,cell)
     }
-    pen.fillStyle='green'
+    pen.font='20px sans-sarif'
+   
+    pen.fillText(`score ${count}`,40,100)
+    
+    pen.fillStyle='#936639'
     pen.fillRect(randomC[0],randomC[1],cell,cell)
 
    }
@@ -47,37 +52,54 @@ let canvas=   document.querySelector("canvas")
  let y=   cellQ[cellQ.length-1][1]
  let newX
  let nexY
- if(direction=='right'){
+ if(direction=='right' ){
     newX=x+cell;
     nexY=y
-    if(newX==800){
+    if(newX==900|| checkmate(newX,nexY)){
         gameOver=true
+         pen.font='20px sans-sarif'
+          pen.fillStyle='red'
+    pen.fillText('GAME OVER',40,120)
+     
     }
  }
  else if(direction=='left'){
     newX=x-cell;
     nexY=y
-    if(newX<0){
+    if(newX<0|| checkmate(newX,nexY)){
         gameOver=true
+ pen.font='20px sans-sarif'
+  pen.fillStyle='red'
+    pen.fillText('GAME OVER',40,120)
+     
     }
  }
  else if(direction=='down'){
     newX=x
     nexY=y+cell
-    if(nexY==400){
+    if(nexY==500|| checkmate(newX,nexY)){
         gameOver=true
+         pen.font='20px sans-sarif'
+         pen.fillStyle='red'
+    pen.fillText('GAME OVER',40,120)
+      
     }
  }
  else{
     newX=x
     nexY=y-cell
-    if(nexY<0){
+    if(nexY<0|| checkmate(newX,nexY)){
         gameOver=true
+         pen.font='20px sans-sarif'
+          pen.fillStyle='red'
+    pen.fillText('GAME OVER',40,120)
+     
     }
  }
 
  if(newX==randomC[0]  && nexY==randomC[1]){
     randomC=generateRandomCell()
+    count=count+1
     
  }
  else{
@@ -88,11 +110,7 @@ let canvas=   document.querySelector("canvas")
 
    }
 
-// let arr=[1,2,3,4];
-//    for(let i of arr ){
-//     console.log(i);
-    
-//    }
+
 
      function generateRandomCell(){
         return[
@@ -100,3 +118,14 @@ let canvas=   document.querySelector("canvas")
             Math.floor(Math.random()*350/50)*50
         ]
      }
+     function checkmate(newX,nexY){
+      for(let i of cellQ){
+         if(i[0]==newX && i[1]==nexY){
+            return true;
+         }
+      }
+      return false
+     }
+     
+
+     
