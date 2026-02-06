6e5564e5b1cc64f1e5e7c4cd8e84c5b0d73ff897
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-02-06 23:29:32 +0530

    initial

--- index9.js
+++ index9.js
@@ -6,11 +6,20 @@
 
 //     core.style.transform = `translate(${x}px, ${y}px)`;
 // });
-const core = document.querySelector('.core');
+// const core = document.querySelector('.core');
+
+// document.addEventListener('mousemove', (e) => {
+//   const x = (window.innerWidth / 2 - e.clientX) / 25;
+//   const y = (window.innerHeight / 2 - e.clientY) / 25;
+
+//   core.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+// });
+const quantum = document.querySelector('.quantum');
 
 document.addEventListener('mousemove', (e) => {
-  const x = (window.innerWidth / 2 - e.clientX) / 25;
-  const y = (window.innerHeight / 2 - e.clientY) / 25;
+  const x = (window.innerWidth / 2 - e.clientX) / 20;
+  const y = (window.innerHeight / 2 - e.clientY) / 20;
 
-  core.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+  quantum.style.transform =
+    `rotateY(${x}deg) rotateX(${y}deg)`;
 });
