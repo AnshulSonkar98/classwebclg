2483ce838cdfe4476aa9813375c43d1533e6b330
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-02-13 23:35:26 +0530

    initial

--- index12.js
+++ index12.js
@@ -1,8 +1,29 @@
-const scene = document.querySelector('.scene');
+// const scene = document.querySelector('.scene');
+
+// document.addEventListener('mousemove', (e) => {
+//   const x = (e.clientX / window.innerWidth - 0.5) * 20;
+//   const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+//   scene.style.transform = `rotateX(${ -y }deg) rotateY(${ x }deg)`;
+// });
+
+
+
+
+
+const light = document.querySelector('.cursor-light');
+const panel = document.querySelector('.panel');
 
 document.addEventListener('mousemove', (e) => {
-  const x = (e.clientX / window.innerWidth - 0.5) * 20;
-  const y = (e.clientY / window.innerHeight - 0.5) * 20;
 
-  scene.style.transform = `rotateX(${ -y }deg) rotateY(${ x }deg)`;
+    /* Light Follow */
+    light.style.left = e.clientX + 'px';
+    light.style.top = e.clientY + 'px';
+
+    /* 3D Tilt */
+    const x = (e.clientX / window.innerWidth - 0.5) * 12;
+    const y = (e.clientY / window.innerHeight - 0.5) * 12;
+
+    panel.style.transform =
+        `rotateX(${ -y }deg) rotateY(${ x }deg)`;
 });
